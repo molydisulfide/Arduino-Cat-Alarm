76cdd4138f0062aae8c02bd9467a15610d33d9dd
Jakub Jadwiszczak <jadwiszj@tcd.ie>
2020-06-21 20:11:52 -0400

    Update SD_card_sketch.ino

--- SD_card_sketch.ino
+++ SD_card_sketch.ino
@@ -5,23 +5,44 @@
 #include "TMRpcm.h"
 #include "SPI.h"
 
-TMRpcm tmrpcm;
 
-void setup()
-{
-tmrpcm.speakerPin=9;
-Serial.begin(9600);
-if(!SD.begin(SD_ChipSelectPin))
-{
-  Serial.println("SD failed to connect!");
-  return;
-}
-tmrpcm.setVolume(5);
-tmrpcm.play("birdo.wav");
+TMRpcm audio;
+
+void setup(){
+  audio.speakerPin=9;
+  Serial.begin(9600);
+  Serial.println("Initializing SD card...");
+
+    if(!SD.begin(SD_ChipSelectPin))
+      {
+        Serial.println("SD failed or not inserted.");
+        return;
+      }
+  Serial.println("SD card initialized.");
+  audio.volume(5);
+  audio.play("birdo.wav");
+  audio.loop(1);
+  audio.pause();
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  
+  int trip_signal;
+  trip_signal = analogRead(A5);
+  if(trip_signal > 700)
+    {
+      Serial.println("Signal has tripped.");
+      Serial.println(trip_signal);
+      audio.pause();
+      delay(2000);
+      
+      return;
+    }
 
+
+  
 }
