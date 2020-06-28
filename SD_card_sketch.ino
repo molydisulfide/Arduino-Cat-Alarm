7f242b991fdc27d7851d83e479e4be95111804e3
Jakub Jadwiszczak <jadwiszj@tcd.ie>
2020-06-27 22:05:19 -0400

    Update of trip code to work off pin #3.

--- SD_card_sketch.ino
+++ SD_card_sketch.ino
@@ -5,11 +5,15 @@
 #include "TMRpcm.h"
 #include "SPI.h"
 
-
 TMRpcm audio;
 
+int val = 0; // variable to store the read value.
+int trip_pin = 3; // declare the input pin for tripping the audio signal. 
+
 void setup(){
-  audio.speakerPin=9;
+  
+// Set up SD card connection://
+  
   Serial.begin(9600);
   Serial.println("Initializing SD card...");
 
@@ -18,31 +22,37 @@ void setup(){
         Serial.println("SD failed or not inserted.");
         return;
       }
+      
   Serial.println("SD card initialized.");
+
+// Set up audio parameters for playing the 8-bit birdsong file: //
+
+  audio.speakerPin=9;
   audio.volume(5);
   audio.play("birdo.wav");
   audio.loop(1);
   audio.pause();
 
+// Set up input pin://
 
+ 
+  
+  pinMode(trip_pin, INPUT); // sets the digital pin #3 as input trip signal coming from the output of the ESP board.
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   
+  // This loop checks for digital input on pin #3 and trips the audio file on loop on from the SD card://  
+
+  val = digitalRead(trip_pin); // read the input pin.
   
-  int trip_signal;
-  trip_signal = analogRead(A5);
-  if(trip_signal > 700)
+  if(val > 0)
     {
       Serial.println("Signal has tripped.");
-      Serial.println(trip_signal);
+      Serial.println(val); // print the read-out value to the Serial Monitor.
       audio.pause();
       delay(2000);
       
       return;
     }
-
-
-  
 }
